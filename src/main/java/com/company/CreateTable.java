e7524514588dd83f95e45355440ee7eca35258d8
daviet <viet.do@gbst.com>
2022-04-28 07:40:27 +0700

    install maven and run code

--- src/main/java/com/company/CreateTable.java
+++ src/main/java/com/company/CreateTable.java
@@ -10,19 +10,19 @@ public class CreateTable {
 
 				try {
 						// register driver
-           Class.forName("org.hsqldb.jdbc.JDBCDriver");
+//           Class.forName("org.hsqldb.jdbc.JDBCDriver");
 					 // create connection
 					 con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/testdb","SA","");
 					 // create statement
 					 stmt = con.createStatement();
              // create table
-//					 result = stmt.executeUpdate("CREATE TABLE tutorials_tbl (\n" +
-//							 "   id INT NOT NULL,\n" +
-//							 "   title VARCHAR(50) NOT NULL,\n" +
-//							 "   author VARCHAR(20) NOT NULL,\n" +
-//							 "   submission_date DATE,\n" +
-//							 "   PRIMARY KEY (id) \n" +
-//							 ");");
+					 result = stmt.executeUpdate("CREATE TABLE tutorials_tbl (\n" +
+							 "   id INT NOT NULL,\n" +
+							 "   title VARCHAR(50) NOT NULL,\n" +
+							 "   author VARCHAR(20) NOT NULL,\n" +
+							 "   submission_date DATE,\n" +
+							 "   PRIMARY KEY (id) \n" +
+							 ");");
 						// insert row on the table
 //						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(100,'Learn PHP','John Poul',NOW())");
             // insert many rows
@@ -30,16 +30,16 @@ public class CreateTable {
 //						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(102,'Learn MySQL','Abdul S',NOW())");
 //						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(103,'Learn Excell','Bavya kanna',NOW())");
 //						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(104,'Learn JDB','Ajith kumar',NOW())");
-						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(101,'Learn Junit','Sathya Murthi',NOW())");
+//						result = stmt.executeUpdate("INSERT INTO tutorials_tbl VALUES(101,'Learn Junit','Sathya Murthi',NOW())");
 						// delete table
 //						 result = stmt.executeUpdate("DROP TABLE tutorials_tbl");
 						// Execute queries
 //						PreparedStatement ps = con.prepareStatement("Select * from tutorials_tbl");
-						ResultSet rs = stmt.executeQuery("select * from tutorials_tbl where id = 102");
-						while(rs.next()) {
-							   String title = rs.getString("title");
-								System.out.println(title);
-						}
+//						ResultSet rs = stmt.executeQuery("select * from tutorials_tbl where id = 102");
+//						while(rs.next()) {
+//							   String title = rs.getString("title");
+//								System.out.println(title);
+//						}
 
 				} catch (Exception e) {
           e.printStackTrace(System.out);
